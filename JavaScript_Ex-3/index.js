7e7a605b82b367434ac72651ddf83e60bfce2903
HarhslaMjn <mahajanh006@gmail.com>
2023-08-05 22:39:06 +0530

    fetch data from an API using the Fetch API

--- JavaScript_Ex-3/index.js
+++ JavaScript_Ex-3/index.js
@@ -110,6 +110,25 @@ function validateForm(event) {
 const form = document.getElementById("myForm");
 form.addEventListener("submit", validateForm);
 
+//6)Write a function to fetch data from an API using the Fetch API and handle the response with a callback.
+function fetchData(url, callback) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(null, data))
+    .catch(error => callback(error, null));
+}
+
+// Example usage
+fetchData('https://jsonplaceholder.typicode.com/todos/1', (error, data) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
+//Note:The Fetch API is built into modern web browsers and allows developers to make HTTP requests to servers using JavaScript.
+
+
 
 
 
